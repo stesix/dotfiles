7e1d34b81bc544efad2c748825d79115606cead1
stesix <stesix@users.noreply.github.com>
2024-04-26 00:07:53 +0200

    chore: fix keymap

---
+++ .xkb/load.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export DISPLAY=${DISPLAY:-:0}
+
+
+xhost local:root
+
+setxkbmap \
+    -layout us,it,de \
+    -variant intl-unicode,,qwerty \
+    -option 'grp:shifts_toggle' \
+    -option 'altwin:swap_lalt_lwin' \
+    -option 'caps:ctrl_modifier' \
+    -option 'lv5:rwin_switch_lock'
+
+xkbcomp -w0 -I$HOME/.xkb $HOME/.xkb/keymap/mykbd $DISPLAY
