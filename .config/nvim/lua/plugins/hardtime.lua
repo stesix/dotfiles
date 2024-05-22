abcb35231114a7fdffcc381829da63a1d8fbef42
stesix <stesix@users.noreply.github.com>
2024-05-22 10:06:30 +0200

    chore(nvim:plugin): add hardtime

---
+++ .config/nvim/lua/plugins/hardtime.lua
@@ -0,0 +1,11 @@
+return {
+  "m4xshen/hardtime.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "nvim-lua/plenary.nvim",
+  },
+  opts = {
+    showmode = false,
+  },
+}
