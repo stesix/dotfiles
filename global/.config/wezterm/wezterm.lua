48e168ee24fab52e6425008795f2cabfd3ddbbe0
stesix <stesix@users.noreply.github.com>
2024-12-02 23:35:59 +0100

    feature(wezterm:osx): add env variables

--- global/.config/wezterm/wezterm.lua
+++ global/.config/wezterm/wezterm.lua
@@ -6,11 +6,20 @@ local session_manager = require('plugins.session-manager')
 -- local resurrect = require('plugins.resurrect')
 
 local config = wezterm.config_builder()
+local is_linux = wezterm.target_triple == 'x86_64-unknown-linux-gnu'
+local is_osx = wezterm.target_triple == 'aarch64-apple-darwin'
 
 config.unix_domains = { { name = 'unix' } }
 config.default_gui_startup_args = { 'connect', 'unix' }
 config.default_prog = { '/opt/homebrew/bin/bash', '--login' }
 config.color_scheme = 'Catppuccin Mocha'
+
+if is_osx then
+  config.set_environment_variables = {
+    XDG_CONFIG_HOME = '/Users/' .. (os.getenv('USERNAME') or os.getenv('USER')) .. '/.config',
+    HOMEBREW_PREFIX = '/opt/homebrew',
+  }
+end
 
 url.apply(config)
 mwindow.apply(config)
