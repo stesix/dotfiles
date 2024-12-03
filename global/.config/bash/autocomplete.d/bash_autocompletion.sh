5368211d0526eb982e653fdeb78269cbc8ba5ed0
stesix <stesix@users.noreply.github.com>
2024-12-03 16:26:18 +0100

    feature(bash): add autocomplete

---
+++ global/.config/bash/autocomplete.d/bash_autocompletion.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [ -n "$HOMEBREW_PREFIX" ] ; then
+  if [[ -r "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh" ]]
+  then
+    source "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh"
+  else
+    for COMPLETION in "${HOMEBREW_PREFIX}/etc/bash_completion.d/"*
+    do
+      [[ -r "${COMPLETION}" ]] && source "${COMPLETION}"
+    done
+  fi
+fi
