ebb39bab9b7075874f08d3f1710ddecd0c30d83e
stesix <stesix@users.noreply.github.com>
2024-12-18 11:12:37 +0100

    fix(bash:azdo): add missing parameter

--- global/.config/bash/functions.d/azdo.sh
+++ global/.config/bash/functions.d/azdo.sh
@@ -2,6 +2,6 @@
 
 # Load AzureDevOps credentials into environment
 function azdo_auth() {
-    export AZURE_ACCESS_TOKEN="$(keepassxc-cli show --key-file ${KEEPASS_KEY} -a PASSWORD ${KEEPASS_DB} )"
+    export AZURE_ACCESS_TOKEN="$(keepassxc-cli show --key-file ${KEEPASS_KEY} -a PASSWORD ${KEEPASS_DB} ${KEEPASS_AZDO_NAME})"
     export PIP_EXTRA_INDEX_URL="${AZURE_USER_NAME}:${AZURE_ACCESS_TOKEN}@${AZDO_CHANNEL}"
 }
