72057ece6f85e275e07128479a44573f7aef4801
stesix <stesix@users.noreply.github.com>
2025-03-24 22:34:35 +0100

    perf(bash): add azdo_openai function

--- global/.config/bash/functions.d/azdo.sh
+++ global/.config/bash/functions.d/azdo.sh
@@ -5,3 +5,7 @@ function azdo_auth() {
     export AZURE_ACCESS_TOKEN="$(keepassxc-cli show --key-file ${KEEPASS_KEY} -a PASSWORD ${KEEPASS_DB} ${KEEPASS_AZDO_NAME})"
     export PIP_EXTRA_INDEX_URL="${AZURE_USER_NAME}:${AZURE_ACCESS_TOKEN}@${AZDO_CHANNEL}"
 }
+
+function azdo_openai() {
+    export AZURE_API_KEY="$(keepassxc-cli show --key-file ${KEEPASS_KEY} -a PASSWORD ${KEEPASS_DB} ${KEEPASS_AZOPENAI_NAME})"
+}
