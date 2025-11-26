04d71f59cf409f793ab257f6bd0522b5b3f74d10
stesix <stesix@users.noreply.github.com>
2025-11-26 11:19:21 +0100

    fix(bash): add protocol in azdo_auth function

--- global/.config/bash/functions.d/azdo.sh
+++ global/.config/bash/functions.d/azdo.sh
@@ -3,11 +3,12 @@
 # Load AzureDevOps credentials into environment
 function azdo_auth() {
     export AZURE_ACCESS_TOKEN="$(keepassxc-cli show --key-file ${KEEPASS_KEY} -a PASSWORD ${KEEPASS_DB} ${KEEPASS_AZDO_NAME})"
-    export PIP_EXTRA_INDEX_URL="${AZURE_USER_NAME}:${AZURE_ACCESS_TOKEN}@${AZDO_CHANNEL}"
+    export PIP_EXTRA_INDEX_URL="https://${AZURE_USER_NAME}:${AZURE_ACCESS_TOKEN}@${AZDO_CHANNEL}"
 }
 
 function azdo_openai() {
     export AZURE_API_KEY="$(keepassxc-cli show --key-file ${KEEPASS_KEY} -a PASSWORD ${KEEPASS_DB} ${KEEPASS_AZOPENAI_NAME})"
+    export AZURE_OPENAI_API_KEY="${AZURE_API_KEY}"
 }
 
 function azdo_terraform() {
