1ea37fd85e635cd4cc8485538cb001ce212b79f4
stesix <stesix@users.noreply.github.com>
2026-02-24 13:59:07 +0100

    perf(nvim): make background transparent

--- global/.config/nvim/lua/plugins/themes.lua
+++ global/.config/nvim/lua/plugins/themes.lua
@@ -1,9 +1,17 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
+  'catppuccin/nvim',
+  name = 'catppuccin',
   priority = 1000,
   init = function()
-    vim.cmd.colorscheme("catppuccin-mocha")
-    vim.cmd.hi("Comment gui=none")
+    vim.cmd.colorscheme('catppuccin-mocha')
+    vim.cmd.hi('Comment gui=none')
+
+    -- Make background transparent
+    vim.cmd([[
+      highlight Normal guibg=none
+      highlight NonText guibg=none
+      highlight Normal ctermbg=none
+      highlight NonText ctermbg=none
+    ]])
   end,
 }
