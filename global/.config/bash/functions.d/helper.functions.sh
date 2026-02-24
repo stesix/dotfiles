1bd7d2a932f5b324f455b96bf33349d459a97dd4
stesix <stesix@users.noreply.github.com>
2026-02-24 22:05:53 +0100

    fix(bash): add wl-clipboard for copying

--- global/.config/bash/functions.d/helper.functions.sh
+++ global/.config/bash/functions.d/helper.functions.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 function __copy() {
-    if hash pbcopy 2>/dev/null; then
+    if hash pbcopy &>/dev/null; then
         pbcopy
-    elif hash wl-copy &>/dev/null ; then
+    elif hash wl-copy &>/dev/null; then
         wl-copy
-    elif hash clip.exe 2>/dev/null; then
+    elif hash clip.exe &>/dev/null; then
         clip.exe
-    elif hash xclip 2>/dev/null; then
+    elif hash xclip &>/dev/null; then
         xclip -selection clip
     else
         cat
