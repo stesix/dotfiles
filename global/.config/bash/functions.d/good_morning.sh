6fdd7e7fb2c14735f51728e0d22f4a6ff9b53ae8
stesix <stesix@users.noreply.github.com>
2026-03-04 09:37:05 +0100

    perf(bash): add the good_morning_bitch function

---
+++ global/.config/bash/functions.d/good_morning.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+function good_morning_bitch {
+    local bmad_version exit_code
+    local DATA_DIR="${XDG_DATA_HOME}/good_morning_bitch"
+    local bmad_version_file="${DATA_DIR}/bmad_version.txt"
+
+    mkdir -p "$DATA_DIR"
+
+    brew bundle -g --upgrade --cleanup
+
+    # Upgrade wezterm
+    brew upgrade --cask wezterm@nightly --greedy-latest
+
+    # Upgrade neovim plugins and LSP
+    nvim --headless "+Lazy update" "+MasonUpdate" "+qa"
+    echo ""
+
+    bmad_version="$(basename "$(curl -s https://github.com/bmad-code-org/BMAD-METHOD/releases/ | grep -o '[^"]*BMAD-METHOD/releases/tag[^"]*' | head -1)")"
+    exit_code=$?
+
+    if [[ $exit_code -gt 0 ]]; then
+        echo "Something went wrong while checking bmad version"
+        return 1
+    fi
+
+    echo "$bmad_version" >$bmad_version_file
+
+    if [[ ! -r "${bmad_version_file}.old" ]] || ! diff -qqq "$bmad_version_file" "${bmad_version_file}.old"; then
+        echo "A new BMAD version ($bmad_version) is now available."
+        cp "$bmad_version_file" "${bmad_version_file}.old" &>/dev/null
+    fi
+}
